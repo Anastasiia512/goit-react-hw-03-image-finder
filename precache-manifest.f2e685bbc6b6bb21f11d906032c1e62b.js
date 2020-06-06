88ed9e3759f4415b3067c792750d1e14f638f251
Anastasiia <nastyona221985@gmail.com>
2020-06-06 17:36:59 +0300

    Updates

---
+++ precache-manifest.f2e685bbc6b6bb21f11d906032c1e62b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58714d74c7ca8628ea50956fee932456",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "e72d06b916280d43018f",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.e76bf913.chunk.css"
+  },
+  {
+    "revision": "9159b8ef22bf2cb04eb7",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.fcbdc095.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.fcbdc095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72d06b916280d43018f",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.141f5a20.chunk.js"
+  },
+  {
+    "revision": "25868c5f5ac532d6d81a",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.9cda7063.js"
+  }
+]);
